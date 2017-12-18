270cce4b529295689e28b53e33ab3cc7cc1b7bc5
Tejas Chheda <tejas4888@gmail.com>
2017-12-18 18:24:36 +0530

    Added marker for Featured Events

--- app/src/main/java/sp/udaan/Activites/MapsActivity.java
+++ app/src/main/java/sp/udaan/Activites/MapsActivity.java
@@ -23,7 +23,7 @@ import sp.udaan.R;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
-    private Marker marker1,marker2,marker3;
+    private Marker marker1,marker2,marker3,marker4;
     private Marker prev_marker;
     static int tracker=0;
     @Override
@@ -63,11 +63,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng location1 = new LatLng(19.229646, 72.843572);
         LatLng location2=new LatLng(19.1219362,72.8432649);
         LatLng location3=new LatLng(19.003302, 72.840541);
+        LatLng location4=new LatLng(19.0556692,72.8158758);
         marker1=mMap.addMarker(new MarkerOptions().position(location1).title("Borivali").snippet("Fun Events"));
         marker1.showInfoWindow();
         marker2=mMap.addMarker(new MarkerOptions().position(location2).title("SPIT").snippet("Performing Arts"));
         marker2.showInfoWindow();
         marker3=mMap.addMarker(new MarkerOptions().position(location3).title("Worli").snippet("Literary Arts"));
+        marker3.showInfoWindow();
+        marker4=mMap.addMarker(new MarkerOptions().position(location4).title("Bandra").snippet("Featured"));
+        marker4.showInfoWindow();
 
         CameraPosition cameraPosition = new CameraPosition.Builder().target(
                 new LatLng(19.1112823,72.892324)).zoom((float) 10.5).build();
@@ -86,6 +90,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 prev_marker=marker2;
             }   else if (marker.equals(marker3)) {
                 prev_marker=marker3;
+            }   else if (marker.equals(marker4)){
+                prev_marker=marker4;
             }
         }else{
             Intent i=new Intent(MapsActivity.this,MainActivity.class);
@@ -98,6 +104,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 i.putExtra("EventCategory","Performing Arts");
             }   else if (marker.equals(marker3)) {
                 i.putExtra("EventCategory","Literary Arts");
+            }   else if (marker.equals(marker4)) {
+                i.putExtra("EventCategory","Featured");
             }
             startActivity(i);
         }
