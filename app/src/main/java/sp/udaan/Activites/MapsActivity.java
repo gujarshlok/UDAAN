1b4ca3c6767d3823228da784c3d9c46365afea0b
Tejas Chheda <tejas4888@gmail.com>
2017-12-18 17:08:09 +0530

    Map double clicked with categories and zoom restriction

--- app/src/main/java/sp/udaan/Activites/MapsActivity.java
+++ app/src/main/java/sp/udaan/Activites/MapsActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -33,6 +34,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        Toast.makeText(this,"Hle",Toast.LENGTH_SHORT).show();
     }
 
 
@@ -50,6 +52,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
+        //Added restrictions on zoom
+        mMap.setMaxZoomPreference((float)14);
+        mMap.setMinZoomPreference((float)10.5);
+
         mMap.setOnMarkerClickListener(this);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
@@ -58,11 +64,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng location1 = new LatLng(19.229646, 72.843572);
         LatLng location2=new LatLng(19.1219362,72.8432649);
         LatLng location3=new LatLng(19.003302, 72.840541);
-        marker1=mMap.addMarker(new MarkerOptions().position(location1).title("Borivali").snippet("Floor 1"));
+        marker1=mMap.addMarker(new MarkerOptions().position(location1).title("Borivali").snippet("Fun Events"));
         marker1.showInfoWindow();
-        marker2=mMap.addMarker(new MarkerOptions().position(location2).title("SPIT").snippet("Floor 2"));
+        marker2=mMap.addMarker(new MarkerOptions().position(location2).title("SPIT").snippet("Performing Arts"));
         marker2.showInfoWindow();
-        marker3=mMap.addMarker(new MarkerOptions().position(location3).title("Worli").snippet("Floor 3"));
+        marker3=mMap.addMarker(new MarkerOptions().position(location3).title("Worli").snippet("Literary Arts"));
 
         CameraPosition cameraPosition = new CameraPosition.Builder().target(
                 new LatLng(19.1112823,72.892324)).zoom((float) 10.5).build();
@@ -73,34 +79,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public boolean onMarkerClick(Marker marker) {
 
-        if( marker.equals(marker1))
-        {
-            Intent i= new Intent(MapsActivity.this,MainActivity.class);
+        if (tracker==0 || !prev_marker.equals(marker)) {
+            tracker++;
+            if (marker.equals(marker1)) {
+                prev_marker=marker1;
+            }   else if (marker.equals(marker2)) {
+                prev_marker=marker2;
+            }   else if (marker.equals(marker3)) {
+                prev_marker=marker3;
+            }
+        }else{
+            Intent i=new Intent(MapsActivity.this,MainActivity.class);
+            tracker=0;
+            if (marker.equals(marker1)) {
+                prev_marker=marker1;
+                i.putExtra("FloorNo","1");
+            }   else if (marker.equals(marker2)) {
+                prev_marker=marker2;
+                i.putExtra("FloorNo","2");
+            }   else if (marker.equals(marker3)) {
+                i.putExtra("FloorNo","3");
+            }
             startActivity(i);
         }
-//        if (tracker==0 || !prev_marker.equals(marker)) {
-//            tracker++;
-//            if (marker.equals(marker1)) {
-//                prev_marker=marker1;
-//            }   else if (marker.equals(marker2)) {
-//                prev_marker=marker2;
-//            }   else if (marker.equals(marker3)) {
-//                prev_marker=marker3;
-//            }
-//        }else{
-//            Intent i=new Intent(MapsActivity.this,MainActivity.class);
-//            tracker=0;
-//            if (marker.equals(marker1)) {
-//                prev_marker=marker1;
-//                i.putExtra("FloorNo","1");
-//            }   else if (marker.equals(marker2)) {
-//                prev_marker=marker2;
-//                i.putExtra("FloorNo","2");
-//            }   else if (marker.equals(marker3)) {
-//                i.putExtra("FloorNo","3");
-//            }
-//            startActivity(i);
-//        }
         return false;
     }
 }
